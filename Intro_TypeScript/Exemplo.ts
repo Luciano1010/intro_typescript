9a78fa3e5ff9b7a2ce6e0d5157079129612a5b3a
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-10-20 11:27:48 -0300

    intro_typescript

---
+++ Intro_TypeScript/Exemplo.ts
@@ -0,0 +1,8 @@
+function add(num1: number,num2: number){
+        console.log(num1+num2)
+}
+
+
+add(10, 15)
+// add(false, true)
+//add("goiaba", true)
---
+++ Intro_TypeScript/Objeto.ts
@@ -0,0 +1,37 @@
+interface TurmaModelo {
+    nome: string
+    qtd:number
+    materia:string
+    instrutor:string
+}
+
+
+const Turma3: TurmaModelo = { 
+    nome: "Turma.NET",
+    qtd:32,
+    materia: "FullStack c# jr",
+    instrutor: "Yuri"
+}
+console.log(Turma3)
+
+
+
+
+
+const Turma: Object = { 
+    nome: "Turma.NET",
+    qtd:32,
+    materia: "FullStack c# jr"
+}
+
+console.log(Turma)
+
+
+
+const Turma2: Object = { 
+    nome: "Turma.NET",
+    qtd:32,
+    materia: "FullStack c# jr"
+}
+
+console.log(Turma2)
